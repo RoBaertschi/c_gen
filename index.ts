1a3a98b9f7cb07c7b463d319a0a1f7b527caea99
Robin Bärtschi <rtmbaertschi007@gmail.com>
2025-04-03 22:34:04 +0200

    begin

--- index.ts
+++ index.ts
@@ -1,17 +1,163 @@
-import
-console.log(process.argv)
+// @ts-check
+
+import { readFileSync, writeFileSync } from 'node:fs';
 const actualArgs = process.argv.slice(2)
 const input = actualArgs[0]
 const output = actualArgs[1]
 
 if (!input) {
-	console.error("missing first argument for input")
-	process.exit(1)
+    console.error("missing first argument for input")
+    process.exit(1)
 }
 
 if (!output) {
-	console.error("missing first argument for output")
-	process.exit(1)
+    console.error("missing first argument for output")
+    process.exit(1)
 }
 
-const content = reafile
+const content = readFileSync(input).toString("utf8");
+const jsonContent = JSON.parse(content)
+
+const arrays: string[] = [];
+
+for (const key in jsonContent) {
+    const content = jsonContent[key]
+    if (key == "arrays") {
+        if (Array.isArray(content)) {
+            for (let i = 0; i < content.length; i++) {
+                const arrElement = content[i]
+                if (typeof arrElement == "string") {
+                    arrays.push(arrElement)
+                } else {
+                    console.error(`INVALID ELEMENT IN "${key}", expected string, got ${typeof arrElement}`)
+                    process.exit(1)
+                }
+            }
+        }
+    } else {
+        console.error(`INVALID KEY "${key}"`)
+        process.exit(1)
+    }
+}
+
+let outputText = `
+#include <stddef.h>
+#include <stdlib.h>
+#include <assert.h>
+enum array_err {
+    ARRAY_OK,
+    ARRAY_OOM
+};
+
+typedef enum array_err array_err;
+`
+
+for (let i = 0; i < arrays.length; i++) {
+    const e = arrays[i]
+    if (!e) {
+        throw new Error("FUCK");
+    }
+    const niceName = e?.replaceAll("*", "_ptr").replaceAll(" ", "_").replaceAll(/_+/g, "_")
+    const arrayName = "array_" + niceName;
+    const arrayNameUpperCase = arrayName.toUpperCase();
+    const sliceName = "slice_" + niceName;
+    const initialSize = 4;
+
+    outputText +=
+        `
+
+struct ${sliceName} {
+    ${e} *items;
+    size_t len;
+};
+
+typedef struct ${sliceName} ${sliceName};
+
+struct ${arrayName} {
+    ${e} *items;
+    size_t len;
+    size_t cap;
+};
+
+typedef struct ${arrayName} ${arrayName};
+
+${sliceName} ${arrayName}_slice(${arrayName} *arr, size_t from, size_t to) {
+    assert(0 <= from);
+    assert(to <= arr->len);
+    assert(to < from);
+    return (${sliceName}){
+        .items = arr->items + from,
+        .len = to,
+    };
+}
+
+array_err ${arrayName}_grow(${arrayName} *arr) {
+    if (arr->items == NULL) {
+        arr->items = malloc(sizeof(arr->items[0]) * ${initialSize});
+        if (arr->items == NULL) {
+            return ARRAY_OOM;
+        }
+        arr->cap = ${initialSize};
+    } else {
+        size_t new_cap = sizeof(arr->items[0]) * arr->cap * 2;
+        ${e}* items = realloc(arr->items, new_cap);
+        if (arr->items == NULL) {
+            return ARRAY_OOM;
+        }
+        arr->items = items;
+        arr->cap = new_cap;
+    }
+    return ARRAY_OK;
+}
+
+array_err ${arrayName}_grow_until(${arrayName} *arr, size_t until) {
+    while(arr->cap < until) {
+        array_err err = ${arrayName}_grow(arr);
+        if (err != ARRAY_OK) {
+            return err;
+        }
+    }
+    return ARRAY_OK;
+}
+
+array_err ${arrayName}_push(${arrayName} *arr, ${e} item) {
+    if (arr->cap <= arr->len) {
+        array_err err = ${arrayName}_grow_until(arr, arr->len + 1);
+        if (err != ARRAY_OK) {
+            return err;
+        }
+    }
+    arr->items[arr->len] = item;
+    arr->len += 1;
+
+    return ARRAY_OK;
+}
+
+array_err ${arrayName}_append(${arrayName} *arr, ${sliceName} slice) {
+    size_t new_size = slice.len + arr->len;
+    if (arr->cap <= slice.len + arr->len) {
+        array_err err = ${arrayName}_grow_until(arr, new_size);
+        if (err != ARRAY_OK) {
+            return err;
+        }
+    }
+    for (size_t i = 0; i < slice.len; i++) {
+        arr->items[arr->len + i] = slice.items[i];
+    }
+    arr->len += slice.len;
+    return ARRAY_OK;
+}
+
+#define ${arrayNameUpperCase}_APPEND(arr, ...) ${arrayName}_append((arr), (${sliceName}){ .items = (${e}[]) { __VA_ARGS__ }, .len = sizeof((${e}[]){ __VA_ARGS__ }[0]) })
+
+void test_${niceName}(void) {
+    ${arrayName} arr = {0};
+    ${arrayNameUpperCase}_APPEND(&arr, 0, 0);
+    ${arrayNameUpperCase}_APPEND(&arr, 0);
+}
+
+`
+}
+
+writeFileSync(output, outputText)
+
